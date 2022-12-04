627bf01b06e5c79b92752caa4aa2e985981bd773
Mykhailo <mykhailo@Mykhailos-MBP-2.lan>
2022-12-04 14:58:21 +0200

    Initial commit

---
+++ bin/aoc18_dart.dart
@@ -0,0 +1,5 @@
+import 'package:aoc18_dart/aoc18_dart.dart' as aoc18_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aoc18_dart.calculate()}!');
+}
---
+++ test/aoc18_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc18_dart/aoc18_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
